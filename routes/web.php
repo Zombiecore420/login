8db5ecbad507762c978fe9c12783632b3ed85743
Alex <al221810641@gmail.com>
2021-03-06 21:45:56 -0600

    archivos

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/login-two-factor/{user}', 'Auth\LoginController@login2FA')->name('login.2fa');
+Auth::routes();
+
+
